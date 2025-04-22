3a1c90de571c8afd08fdb4e67c491cf583bb9c42
pika-online <605686962@qq.com>
2025-04-23 01:25:59 +0800

    1111

--- funsound/config.py
+++ funsound/config.py
@@ -4,7 +4,7 @@ model_dir = "models"
 #    LLM 账号
 #################
 llm_account = {
-    "api_key": "", # openai 请求范式
+    "api_key": "sk-VwGMQ0RlgLVRk2BN54o7ITHPAkjZyoxFnBR9YvAMz28VJirb",
     "url": "https://api.moonshot.cn/v1/chat/completions",
     "model_id": "moonshot-v1-128k"
 }
